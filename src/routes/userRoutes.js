6b3e1daa081611ad22e3f65f69e493311c65b07b
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-03-18 05:18:14 -0300

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -11,6 +11,7 @@ import {
   changePassword,
   resetPassword,
   forgotPassword,
+  logoutUser
 } from "../controllers/userController.js";
 import { upload } from "../utils/multerConfig.js"; // Import the image upload middleware
 import { authorizeRoles } from "../utils/authorizeRole.js";
@@ -21,6 +22,7 @@ const router = express.Router();
 router.post("/register", authorizeRoles(["ADMIN"]), upload.single("file"), registerUser);
 router.get("/auth/verify-email", verifyEmail);
 router.post("/login", loginUser);
+router.post("/logout", logoutUser);
 router.get("/", authorizeRoles(["ADMIN"]), getAllUsers);
 router.get("/:id", authorizeRoles(["ADMIN", "ROOM_MANAGER", "RECEPTIONIST"]), getUserById);
 router.put("/:id", authorizeRoles(["ADMIN", "ROOM_MANAGER", "RECEPTIONIST"]),  updateUser);
