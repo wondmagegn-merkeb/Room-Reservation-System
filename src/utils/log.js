a28f68b43ecb7c09a74dc3b44be8b525cb2adffd
wondmagegn-merkeb <146819785+wondmagegn-merkeb@users.noreply.github.com>
2025-04-02 08:07:11 +0300

    Update log.js

--- src/utils/log.js
+++ src/utils/log.js
@@ -2,11 +2,11 @@ import { prisma } from "../prismaClient.js";
 
 // Helper function to log operations
 export const logOperation = async (category, description, performedBy) => {
-  await prisma.log.create({
-    data: {
-      category,
-      description,
-      performedBy,
-    },
-  });
+ // await prisma.log.create({
+  //  data: {
+   //   category,
+      //description,
+   //   performedBy,
+   // },
+  //});
 };
